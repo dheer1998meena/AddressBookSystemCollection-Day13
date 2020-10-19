7ea5d19f6ac0051e5a5ab4d5767fbfaf44046fd2
dheer meena <dheer1998meena@gmail.com>
2020-10-19 11:57:35 +0530

    [Dheer] Added UC7-UC10 by using collection method

---
+++ AddressBook.cs
@@ -0,0 +1,102 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AddressBook.cs" company="Bridgelabz">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Dheer Singh Meena"/>
+// --------------------------------------------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookSystemCollection
+{
+    public class AddressBook
+    {
+        //creating a new list to store contact details entered by the user
+        private List<Contact> list = new List<Contact>();
+        //create a dictionary(generic collection) to store keyvalue pair
+        private Dictionary<string, Contact> d = new Dictionary<string, Contact>();
+        public List<Contact> GetList()
+        {
+            return list;
+        }
+        public Dictionary<string, Contact> GetDictionary()
+        {
+            return d;
+        }
+        //method to add address to the list
+        public void AddAddress(string kname, Contact c)
+        {
+            list.Add(c);
+            d.Add(kname, c);
+
+        }
+        //method to view contact details using key name
+        public Contact ViewByKeyName(string kname)
+        {
+            //iterating the keyvalue pair using for each loop
+            foreach (KeyValuePair<string, Contact> kvp in d)
+            {
+                if (kvp.Key == kname)
+                    return kvp.Value;
+            }
+            return null;
+        }
+        public List<Contact> ViewAddressBook()
+        {
+            return list;
+        }
+        //method to edit the contact details
+        public void EditNumber(string ename, string newnumber)
+        {
+            Boolean flag = false;
+            foreach (Contact cc in list)
+            {
+                if (cc.GetName().Equals(ename))
+                {
+                    flag = true;
+                    cc.SetPhoneNo(newnumber);
+                    Console.WriteLine("Number edited successfully");
+                    break;
+                }
+            }
+            if (!flag)
+            {
+                Console.WriteLine("No such name found!!!");
+            }
+
+
+        }
+        //method to delete the contact from the phonebook
+        public void RemoveContact(string rname)
+        {
+            Boolean flag = false;
+            foreach (Contact cc in list)
+            {
+                if (cc.GetName().Equals(rname))
+                {
+                    flag = true;
+                    list.Remove(cc);
+                    Console.WriteLine("Number removed successfully");
+                    break;
+                }
+            }
+            if (!flag)
+            {
+                Console.WriteLine("No such name found!!!");
+            }
+        }
+        //method to check for any duplicate entry 
+        public bool UC7_CheckForDuplicateEntry(string name)
+        {
+            foreach (Contact c in list)
+            {
+                if (c.GetName().Equals(name))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
